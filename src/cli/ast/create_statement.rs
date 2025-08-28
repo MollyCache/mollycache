8179ab08480287d854d97d2be8cf4ad78dd896e5
Fletcher555 <fletcher.dares@gmail.com>
2025-08-28 01:12:56 -0400

    Fixed passing the interpreter by reference.

--- src/cli/ast/create_statement.rs
+++ src/cli/ast/create_statement.rs
@@ -1,5 +1,27 @@
-use crate::cli::{ast::SqlStatement, ast::interpreter::Interpreter};
+use crate::cli::{ast::{interpreter::Interpreter, CreateTableStatement, SqlStatement, SqlStatement::CreateTable}, tokenizer::token::TokenTypes};
 
-pub fn build(interpreter: &Interpreter) -> Result<SqlStatement, String> {
+pub fn build(interpreter: &mut Interpreter) -> Result<SqlStatement, String> {
+    interpreter.advance();
+    match interpreter.current_token() {
+        Some(token) => {
+            match token.token_type {
+                TokenTypes::Table => {
+                    return table_statement(interpreter);
+                },
+                TokenTypes::Index => {
+                    return index_statement(interpreter);
+                },
+                _ => return Err(interpreter.format_error()),
+            }
+        },
+        None => return Err(interpreter.format_error()),
+    }
+}
+
+fn table_statement(interpreter: &mut Interpreter) -> Result<SqlStatement, String> {
+    todo!()
+}
+
+fn index_statement(interpreter: &mut Interpreter) -> Result<SqlStatement, String> {
     todo!()
 }
