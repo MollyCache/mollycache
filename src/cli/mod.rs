0268d146b6a7ea83911546b45eecd23dd8bd03dd
Fletcher555 <fletcher.dares@gmail.com>
2025-08-29 03:25:10 -0400

    Clear the entire screen instead of buggy approach

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -4,9 +4,9 @@ mod table;
 mod tokenizer;
 
 pub fn cli() {
+    clear_screen();
     println!("Welcome to the MollyDB CLI");
     let mut line_count = 1;
-    let mut lines_printed = 0; // Start at 1 to account for welcome message
 
     loop {
         print!("({:03}) > ", line_count);
@@ -22,26 +22,23 @@ pub fn cli() {
             break;
         }
         if input.eq_ignore_ascii_case("clear") {
-            let mut total_lines_to_clear = lines_printed + line_count - 1;
-            if total_lines_to_clear > 1 {
-                total_lines_to_clear += line_count-2;
-            }
-            for _ in 0..total_lines_to_clear {
-                print!("\x1B[1A\x1B[2K");
-            }
-            print!("\r\x1B[2K");
+            clear_screen();
+            println!("Welcome to the MollyDB CLI");
             line_count = 1;
-            lines_printed = 0;
             continue;
         }
 
         let tokens = tokenizer::tokenize(input);
         println!("{:?}", tokens);
-        lines_printed += 1;
         let ast = ast::generate(tokens);
         for result in ast {
             println!("{:?}", result);
-            lines_printed += 1;
         }
     }
 }
+fn clear_screen() {
+    // Clear screen and move cursor to top-left
+    print!("\x1B[2J\x1B[1;1H");
+    io::Write::flush(&mut io::stdout()).unwrap();
+}
+
