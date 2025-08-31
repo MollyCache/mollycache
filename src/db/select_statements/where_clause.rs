4c05a859203507e78d6dcf77ef9366eabde3e8a2
Fletcher555 <fletcher.dares@gmail.com>
2025-08-31 13:50:15 -0400

    Add additional test to where clause to cover all logic

--- src/db/select_statements/where_clause.rs
+++ src/db/select_statements/where_clause.rs
@@ -16,7 +16,7 @@ pub fn matches_where_clause(table: &Table, row: &Vec<Value>, where_clause: &Wher
         },
         _ => {
             match column_value.get_type() {
-                DataType::Integer | DataType::Real => {
+                DataType::Integer | DataType::Real | DataType::Text => {
                     match where_clause.operator {
                         Operator::LessThan => {
                             return *column_value < where_clause.value;
@@ -83,6 +83,64 @@ mod tests {
         ]);
         let row = vec![Value::Integer(1)];
         let where_clause = WhereClause {column:"id".to_string(),operator:Operator::Equals,value:Value::Text("Fletcher".to_string())};
+        assert!(!matches_where_clause(&table, &row, &where_clause));
+    }
+
+    #[test]
+    fn matches_where_clause_handles_different_operators() {
+        let table = Table::new("users".to_string(), vec![
+            ColumnDefinition {name:"id".to_string(),data_type:DataType::Integer, constraints: vec![] },
+        ]);
+        let row = vec![Value::Integer(10)];
+        let where_clause = WhereClause {column:"id".to_string(),operator:Operator::GreaterThan,value:Value::Integer(0)};
+        assert!(matches_where_clause(&table, &row, &where_clause));
+        let where_clause = WhereClause {column:"id".to_string(),operator:Operator::GreaterEquals,value:Value::Integer(0)};
+        assert!(matches_where_clause(&table, &row, &where_clause));
+        let where_clause = WhereClause {column:"id".to_string(),operator:Operator::LessThan,value:Value::Integer(20)};
+        assert!(matches_where_clause(&table, &row, &where_clause));
+        let where_clause = WhereClause {column:"id".to_string(),operator:Operator::LessEquals,value:Value::Integer(20)};
+        assert!(matches_where_clause(&table, &row, &where_clause));
+        let where_clause = WhereClause {column:"id".to_string(),operator:Operator::NotEquals,value:Value::Integer(10)};
+        assert!(!matches_where_clause(&table, &row, &where_clause));
+    }
+
+    #[test]
+    fn matches_where_clause_handles_string_comparison() {
+        let table = Table::new("users".to_string(), vec![
+            ColumnDefinition {name:"name".to_string(),data_type:DataType::Text, constraints: vec![] },
+        ]);
+        let row = vec![Value::Text("lop".to_string())];
+        let where_clause = WhereClause {column:"name".to_string(),operator:Operator::GreaterEquals,value:Value::Text("abc".to_string())};
+        assert!(matches_where_clause(&table, &row, &where_clause));
+        let where_clause = WhereClause {column:"name".to_string(),operator:Operator::LessEquals,value:Value::Text("lop".to_string())};
+        assert!(matches_where_clause(&table, &row, &where_clause));
+        let where_clause = WhereClause {column:"name".to_string(),operator:Operator::GreaterThan,value:Value::Text("xyz".to_string())};
+        assert!(!matches_where_clause(&table, &row, &where_clause));
+        let where_clause = WhereClause {column:"name".to_string(),operator:Operator::LessThan,value:Value::Text("abc".to_string())};
+        assert!(!matches_where_clause(&table, &row, &where_clause));
+        let where_clause = WhereClause {column:"name".to_string(),operator:Operator::NotEquals,value:Value::Text("abc".to_string())};
+        assert!(matches_where_clause(&table, &row, &where_clause));
+        let where_clause = WhereClause {column:"name".to_string(),operator:Operator::Equals,value:Value::Text("lop".to_string())};
+        assert!(matches_where_clause(&table, &row, &where_clause));
+    }
+
+    #[test]
+    fn matches_where_clause_handles_null() {
+        let table = Table::new("users".to_string(), vec![
+            ColumnDefinition {name:"id".to_string(),data_type:DataType::Integer, constraints: vec![] },
+        ]);
+        let row = vec![Value::Null];
+        let where_clause = WhereClause {column:"id".to_string(),operator:Operator::GreaterEquals,value:Value::Integer(1)};
+        assert!(!matches_where_clause(&table, &row, &where_clause));
+    }
+
+    #[test]
+    fn matches_where_clause_handles_invalid_operator_for_data_type() {
+        let table = Table::new("users".to_string(), vec![
+            ColumnDefinition {name:"id".to_string(),data_type:DataType::Blob, constraints: vec![] },
+        ]);
+        let row = vec![Value::Blob(vec![1, 2, 3])];
+        let where_clause = WhereClause {column:"id".to_string(),operator:Operator::GreaterEquals,value:Value::Blob(vec![1, 2, 3])};
         assert!(!matches_where_clause(&table, &row, &where_clause));
     }
 }
