c2885302ac83385557ef19e7ac27cafde048e5d6
Fletcher555 <fletcher.dares@gmail.com>
2025-09-08 10:45:52 -0400

    Add tests / update tests for set operators

--- tests/set_operators.rs
+++ tests/set_operators.rs
@@ -48,9 +48,48 @@ fn test_set_operators_order_by_clauses_and_parentheses() {
     let expected_first = vec![
         vec![Value::Integer(2), Value::Text("zane".to_string())],
     ];
-    test_utils::assert_table_rows_eq_unordered(expected_first, result.pop().unwrap().unwrap().unwrap());
-    test_utils::assert_table_rows_eq_unordered(expected_second, result.pop().unwrap().unwrap().unwrap());
+    assert_eq!(expected_first, result.pop().unwrap().unwrap().unwrap());
+    assert_eq!(expected_second, result.pop().unwrap().unwrap().unwrap());
     assert!(result.into_iter().all(|result| result.is_ok() && result.unwrap().is_none()));
+}
+
+#[test]
+fn test_set_operators_with_different_tables_and_clause() {
+    let mut database = Database::new();
+    let sql = "
+    CREATE TABLE users1 (
+        id INTEGER,
+        name TEXT
+    );
+    CREATE TABLE users2 (
+        id INTEGER,
+        name TEXT
+    );
+    CREATE TABLE users3 (
+        employee_id INTEGER,
+        name TEXT
+    );
+    INSERT INTO users1 (id, name) VALUES (1, 'John'), (2, 'Jane'), (3, 'Jim'), (4, 'Jack');
+    INSERT INTO users2 (id, name) VALUES (1, 'Fletcher'), (2, 'Jane'), (3, 'Jim'), (4, 'Fletcher');
+    SELECT name FROM users1 UNION SELECT name FROM users2;
+    SELECT * FROM users1 UNION SELECT * FROM users3;
+    ";
+    let mut result = run_sql(&mut database, sql);
+    let first_result = result.pop().unwrap();
+    assert!(first_result.is_err());
+    let expected_second = "Execution Error with statement starting on line 17 \n Error: Columns mismatch between SELECT statements in Union".to_string();
+    assert_eq!(expected_second, first_result.unwrap_err());
+    assert!(result.iter().all(|result| result.is_ok()));
+    let expected_first = vec![
+        vec![Value::Text("John".to_string())],
+        vec![Value::Text("Fletcher".to_string())],
+        vec![Value::Text("Jane".to_string())],
+        vec![Value::Text("Jim".to_string())],
+        vec![Value::Text("Jack".to_string())],
+    ];
+    test_utils::assert_table_rows_eq_unordered(expected_first, result.pop().unwrap().unwrap().unwrap());
+    assert!(result.into_iter().all(|result| result.is_ok() && result.unwrap().is_none()));
+
 }
 
 // ADD TESTS with two seperate tables with different columns and using SELECT *
