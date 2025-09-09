ab0caaeb3c938216c8ebf2fd9ac484e82db68f11
Fletcher555 <fletcher.dares@gmail.com>
2025-09-09 18:50:11 -0400

    Pull where_statement_element into a seperate file

--- src/interpreter/ast/helpers/where_clause/mod.rs
+++ src/interpreter/ast/helpers/where_clause/mod.rs
@@ -1,11 +1,12 @@
 mod where_condition;
 mod expected_token_matches_current;
+mod where_stack_element;
 
 use expected_token_matches_current::{next_expected_token_from_current, WhereClauseExpectedNextToken};
 
 use crate::interpreter::{ast::{
-    helpers::{common::expect_token_type, where_clause::where_condition::get_condition}, 
-    parser::Parser, LogicalOperator, WhereStackElement, WhereStackOperators, Parentheses}};
+    helpers::{common::expect_token_type, where_clause::where_stack_element::get_where_stack_element}, 
+    parser::Parser, WhereStackElement, WhereStackOperators, Parentheses}};
 use crate::interpreter::tokenizer::token::TokenTypes;
 
 // The WhereStack is a the method that is used to store the order of operations with Reverse Polish Notation.
@@ -81,34 +82,7 @@ pub fn get_where_clause(parser: &mut Parser) -> Result<Option<Vec<WhereStackElem
     Ok(Some(where_stack))
 }
 
-fn get_where_stack_element(parser: &mut Parser, operator_stack: &Vec<WhereStackOperators>) -> Result<Option<WhereStackElement>, String> {
-    let token_type = &parser.current_token()?.token_type;    
-    match token_type {
-        TokenTypes::And | TokenTypes::Or | TokenTypes::Not | TokenTypes::LeftParen | TokenTypes::RightParen => {
-            if token_type == &TokenTypes::RightParen && !operator_stack.contains(&WhereStackOperators::Parentheses(Parentheses::Left)) {
-                return Ok(None); // Mismatched parens can be caused by the UNION STATEMENTs.
-            }
-            let where_stack_element = token_type_to_where_stack_element(token_type);
-            parser.advance()?;
-            Ok(Some(where_stack_element))
-        },
-        TokenTypes::Identifier | TokenTypes::IntLiteral | TokenTypes::RealLiteral | TokenTypes::String | TokenTypes::Blob | TokenTypes::Null => {
-            return Ok(Some(WhereStackElement::Condition(get_condition(parser)?)));
-        }
-        _ => return Ok(None),
-    }
-}
 
-fn token_type_to_where_stack_element(token_type: &TokenTypes) -> WhereStackElement {
-    match token_type {
-        TokenTypes::And => WhereStackElement::LogicalOperator(LogicalOperator::And),
-        TokenTypes::Or => WhereStackElement::LogicalOperator(LogicalOperator::Or),
-        TokenTypes::Not => WhereStackElement::LogicalOperator(LogicalOperator::Not),
-        TokenTypes::LeftParen => WhereStackElement::Parentheses(Parentheses::Left),
-        TokenTypes::RightParen => WhereStackElement::Parentheses(Parentheses::Right),
-        _ => unreachable!("Invalid token type for where stack element"),
-    }
-}
 
 #[cfg(test)]
 mod tests {
