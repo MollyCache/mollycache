c0b538f073a69e03a9cba52451d83dd20e75d351
Fletcher555 <fletcher.dares@gmail.com>
2025-09-10 11:22:11 -0400

    Fix linting errors

--- src/interpreter/ast/transaction_statements.rs
+++ src/interpreter/ast/transaction_statements.rs
@@ -4,12 +4,12 @@ use crate::interpreter::ast::helpers::token::expect_token_type;
 
 pub fn build_begin(parser: &mut Parser) -> Result<SqlStatement, String> {
     parser.advance()?;
-    if (expect_token_type(parser, TokenTypes::Deferred).is_ok() || expect_token_type(parser, TokenTypes::SemiColon).is_ok()) {
+    if expect_token_type(parser, TokenTypes::Deferred).is_ok() || expect_token_type(parser, TokenTypes::SemiColon).is_ok() {
         return Ok(SqlStatement::BeginTransaction(BeginStatement::Deferred));
-    } else if (expect_token_type(parser, TokenTypes::Exclusive).is_ok()) {
+    } else if expect_token_type(parser, TokenTypes::Exclusive).is_ok() {
         return Ok(SqlStatement::BeginTransaction(BeginStatement::Exclusive));
     }
-    else if (expect_token_type(parser, TokenTypes::Immediate).is_ok()) {
+    else if expect_token_type(parser, TokenTypes::Immediate).is_ok() {
         return Ok(SqlStatement::BeginTransaction(BeginStatement::Immediate));
     }
     else {
@@ -23,14 +23,14 @@ pub fn build_commit(parser: &mut Parser) -> Result<SqlStatement, String> {
     return Ok(SqlStatement::Commit);
 }
 
-pub fn build_rollback(parser: &mut Parser) -> Result<SqlStatement, String> {
+pub fn build_rollback(_parser: &mut Parser) -> Result<SqlStatement, String> {
     todo!()
 }
 
-pub fn build_savepoint(parser: &mut Parser) -> Result<SqlStatement, String> {
+pub fn build_savepoint(_parser: &mut Parser) -> Result<SqlStatement, String> {
     todo!()
 }
 
-pub fn build_release(parser: &mut Parser) -> Result<SqlStatement, String> {
+pub fn build_release(_parser: &mut Parser) -> Result<SqlStatement, String> {
     todo!()
 }
