af13a7ba3ea81ce2fb2ad81f5804ccfdcd1efb90
Fletcher555 <fletcher.dares@gmail.com>
2025-09-10 12:08:44 -0400

    Finish tests for Transaction statements

--- src/interpreter/ast/transaction_statements.rs
+++ src/interpreter/ast/transaction_statements.rs
@@ -35,11 +35,12 @@ pub fn build_rollback(parser: &mut Parser) -> Result<SqlStatement, String> {
         expect_token_type(parser, TokenTypes::Savepoint)?;
         parser.advance()?;
         expect_token_type(parser, TokenTypes::Identifier)?;
-        Some(parser.current_token()?.value.to_string())
+        let name = parser.current_token()?.value.to_string();
+        parser.advance()?;
+        Some(name)
     } else {
         None
     };
-    parser.advance()?;
     expect_token_type(parser, TokenTypes::SemiColon)?;
     return Ok(SqlStatement::Rollback(RollbackStatement {
         savepoint_name: name,
@@ -103,5 +104,88 @@ mod tests {
             assert_eq!(expected[i], result);
             let _ = parser.advance_past_semicolon();
         }
+    }
+
+    #[test]
+    fn build_commit_with_all_tokens_is_generated_correctly() {
+        // COMMIT; END;
+        let commit_tokens = vec![
+            token(TokenTypes::Commit, "COMMIT"),
+            token(TokenTypes::SemiColon, ";"),
+            token(TokenTypes::End, "END"),
+            token(TokenTypes::SemiColon, ";"),
+        ];
+        let expected = vec![
+            Some(Ok(SqlStatement::Commit)),
+            Some(Ok(SqlStatement::Commit)),
+        ];
+        let mut parser = Parser::new(commit_tokens);
+        for i in 0..2 {
+            let result = parser.next_statement();
+            assert_eq!(expected[i], result);
+            let _ = parser.advance_past_semicolon();
+        }
+    }
+
+    #[test]
+    fn build_rollback_with_all_tokens_is_generated_correctly() {
+        // ROLLBACK; ROLLBACK TO savepoint_name;
+        let rollback_tokens = vec![
+            token(TokenTypes::Rollback, "ROLLBACK"),
+            token(TokenTypes::SemiColon, ";"),
+            token(TokenTypes::Rollback, "ROLLBACK"),
+            token(TokenTypes::To, "TO"),
+            token(TokenTypes::Savepoint, "SAVEPOINT"),
+            token(TokenTypes::Identifier, "savepoint_name"),
+            token(TokenTypes::SemiColon, ";"),
+        ];
+        let expected = vec![
+            Some(Ok(SqlStatement::Rollback(RollbackStatement {
+                savepoint_name: None,
+            }))),
+            Some(Ok(SqlStatement::Rollback(RollbackStatement {
+                savepoint_name: Some("savepoint_name".to_string()),
+            }))),
+        ];
+        let mut parser = Parser::new(rollback_tokens);
+        for i in 0..2 {
+            let result = parser.next_statement();
+            assert_eq!(expected[i], result);
+            let _ = parser.advance_past_semicolon();
+        }
+    }
+
+    #[test]
+    fn build_savepoint_with_all_tokens_is_generated_correctly() {
+        // SAVEPOINT savepoint_name;
+        let savepoint_tokens = vec![
+            token(TokenTypes::Savepoint, "SAVEPOINT"),
+            token(TokenTypes::Identifier, "savepoint_name"),
+            token(TokenTypes::SemiColon, ";"),
+        ];
+        let expected = 
+        Some(Ok(SqlStatement::Savepoint(SavepointStatement {
+                savepoint_name: "savepoint_name".to_string(),
+        })));
+        let mut parser = Parser::new(savepoint_tokens);
+        let result = parser.next_statement();
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn build_release_with_all_tokens_is_generated_correctly() {
+        // RELEASE SAVEPOINT savepoint_name;
+        let release_tokens = vec![
+            token(TokenTypes::Release, "RELEASE"),
+            token(TokenTypes::Savepoint, "SAVEPOINT"),
+            token(TokenTypes::Identifier, "savepoint_name"),
+            token(TokenTypes::SemiColon, ";"),
+        ];
+        let expected = Some(Ok(SqlStatement::Release(ReleaseStatement {
+            savepoint_name: "savepoint_name".to_string(),
+        })));
+        let mut parser = Parser::new(release_tokens);
+        let result = parser.next_statement();
+        assert_eq!(expected, result);
     }
 }
